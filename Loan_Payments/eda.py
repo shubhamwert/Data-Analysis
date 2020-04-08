c2dd556bda54fdb355ae3ff529e5cb41da673bd6
shubhamwert@gmail.com <shubhamwert@gmail.com>
2020-04-08 16:00:30 +0530

    loan dataset various classifiers tested \n few changes in eda

--- Loan_Payments/eda.py
+++ Loan_Payments/eda.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 df=pd.read_csv('data/loandata/Loan payments data.csv')
 
 df.head()
+
+
+df[df['past_due_days'].isnull()]
+
+#So whoever paid the loan is time has past_due_date =na
 df['past_due_days']=df['past_due_days'].fillna(0)
 
 df.info()
@@ -16,7 +21,18 @@ df['Principal'].hist()
 df.columns
 
 df.describe()
+df.isnull().sum()
 
+df[df['paid_off_time'].isnull()==True]
+#So basically if money is not collected paid_off_time is NaT
+df['loan_status']=df['loan_status'].astype('category')
+df['education']=df['education'].astype('category')
+df['Gender']=df['Gender'].astype('category')
+
+date_columns=['effective_date','due_date','paid_off_time']
+
+for columns in date_columns:
+    df[columns]=pd.to_datetime(df[columns])
 #So we have 3 loan status
 
 print(loan_status)
@@ -41,9 +57,16 @@ df['education'].hist()
 
 total_loan=df.groupby(['education'],axis=0).sum()['Principal']
 plt.pie(total_loan,labels=['Bechalor','High School or Below','Master or Above','college'])
-plt.pie(df.groupby(['Gender','education'],axis=0).sum()['Principal'])
 labels=df.groupby(['Gender','education'],axis=0)
 df.groupby(['Gender','education'],axis=0).sum()['Principal'].plot.pie()
 #male takes more loan
 
+#now lets see we are late on loan and hasnt yet paid it
 
+df[df['paid_off_time'].isna() & df['past_due_days']>0] 
+df[df['paid_off_time'].isna() & df['past_due_days']>0].hist() 
+
+print("MONEY IN JEOPARDY:\n",df[df['paid_off_time'].isna() & df['past_due_days']>0]['Principal'].sum())
+
+print("current difference in total money given and unpaid loans",df['Principal'].sum()-df[(df['paid_off_time'].isna() & df['past_due_days']>0)]['Principal'].sum())
+print("paid loan ratio not paid load",df[(df['paid_off_time'].isna() & df['past_due_days']>0)]['Principal'].sum()/df[~(df['paid_off_time'].isna() & df['past_due_days']>0)]['Principal'].sum())
---
+++ Loan_Payments/random_forest.py
@@ -0,0 +1,54 @@
+from sklearn.ensemble import RandomForestClassifier
+from eda import *
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+columns=df.columns
+
+X=df[['Principal', 'terms', 'past_due_days', 'age','Gender']]
+X = pd.concat([X,pd.get_dummies(df['education'])], axis=1)
+X['Gender'].replace(to_replace=['male','female'], value=[0,1],inplace=True)
+X = preprocessing.StandardScaler().fit(X).transform(X)
+Y=df['loan_status'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+
+testResults={}
+#lets begin the tests
+#Random Forest
+
+
+model=RandomForestClassifier()
+model.fit(X_train,y_train)
+y_hat=model.predict(X_test)
+
+print("accuracy of random forest is ",accuracy_score(y_test,y_hat))
+testResults.update({'randomforest':accuracy_score(y_test,y_hat)})
+
+#K clustering
+
+#deciding value of k
+k=5
+
+kNN_model = KNeighborsClassifier(n_neighbors=k).fit(X_train,y_train)
+kNN_model.fit(X_train,y_train)
+
+y_pred=kNN_model.predict(X_test)
+
+print(sum(y_test==y_pred))
+print("accuracy of svm is ",kNN_model.score(X_test,y_test))
+testResults.update({'K_mean Clustering':kNN_model.score(X_test,y_test)})
+
+#SVM
+model=SVC()
+model.fit(X_train,y_train)
+print("SVM HAS SCORE",model.score(X_test,y_test))
+testResults.update({'svm':model.score(X_test,y_test)})
+
+
